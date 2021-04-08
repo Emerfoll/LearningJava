5eaf1e477edcb5539af3249d051dcf39b50f08f8
Emerfoll <66702595+Emerfoll@users.noreply.github.com>
2021-04-08 15:26:23 -0500

    added things I have learned.

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,27 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello World!");
     System.out.println("Hey, it worked!");
+
+    // for characters
+    char firstLetter = 'c';
+    System.out.println(firstLetter);
+
+    // can only be used for whole numbers.
+    int wholeNumber = 11;
+    System.out.println(wholeNumber);
+
+    // double is used to make decimal numbers or very large/small numbers
+    double price = 9.99;
+    System.out.println(price);
+
+    // used to make a string.
+    String newString = "This is now you make a string";
+    System.out.println(newString);
+
+    // boolean is used for true or false
+    boolean likesBeer = true;
+    System.out.println(likesBeer);
+
+
   }
 }
